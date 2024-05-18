02531ca497076ee391961e606ea09f8101699068
Henry Morgan <henri.morgan@outlook.be>
2024-05-18 14:26:38 +0200

    heroku

--- Stocky/settings.py
+++ Stocky/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*", "www.stocky.site", "stocky.site", "stocky-43e6b0b2291f.herokuapp.com"]
 
